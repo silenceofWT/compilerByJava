29148ae7f87ab3a9fc254bf331e35e207fd1cb50
Wang Tao <1284913857@qq.com>
2019-12-13 14:28:00 +0800

    解决词法分析器的bug

--- src/userUI.java
+++ src/userUI.java
@@ -3,7 +3,8 @@ import java.awt.*;
 
 public class userUI extends JFrame{//用户界面
 
-    public userUI(){//构造函数
+    /*public userUI(){
+        //构造函数
         //初始主窗体
         setTitle("简易编译器");//设置主窗体标题
         setSize(1000,600);//设置主窗体的尺寸
@@ -14,15 +15,15 @@ public class userUI extends JFrame{//用户界面
         Container c=getContentPane();    //获取当前窗口的内容窗格
         c.add(tp);
 
-        /*lexicalAnalyzer l = new lexicalAnalyzer();
+        *//*lexicalAnalyzer l = new lexicalAnalyzer();
         fileParseUtils.txtParse();
         l.CharToToken(fileParseUtils.charArr);
         JLabel jl=new JLabel((lexicalAnalyzer.Tokens.get(1)));    //创建一个标签
 
-        c.add(jl);    //将标签组件添加到内容窗格上*/
+        c.add(jl);    //将标签组件添加到内容窗格上*//*
         setVisible(true);//设置主窗体可见
 
-    }
+    }*/
 
 
 
@@ -34,6 +35,6 @@ public class userUI extends JFrame{//用户界面
 
 
     public static void main(String[] args) {
-        new userUI();
+       /* new userUI();*/
     }
 }
